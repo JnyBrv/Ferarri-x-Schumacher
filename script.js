064f200145ed36c95d0ac3552b96ab9f19d4358b
JnyBrv <romwelaljenarroyo.school@gmail.com>
2025-05-29 02:13:08 +0800

    rayo

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.transition-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href');
+        document.body.classList.add('fade-out');
+        setTimeout(() => {
+            window.location.href = href;
+        }, 2000);
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('fade-in');
+});
